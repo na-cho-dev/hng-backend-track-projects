68ca4ceeccd4b7a91cc554546cefeb09e0f38318
NACHO <nachodev369@gmail.com>
2025-01-30 09:42:28 +0100

    Update on server.js: Enabled CORS

--- hng-backend-track-stage-0/server.js
+++ hng-backend-track-stage-0/server.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.get('/', (req, res) => {
     const data = {
